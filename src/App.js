f769ad8878538b290273bd01360a9bd255b5da0c
Vildan Günay <vildan-guenay@hotmail.com>
2023-02-14 15:26:35 +0100

    carousel live projects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './components/Header';
+import { LiveProjects } from './components/LiveProjects';
 
 function App() {
   return (
     <section>
       <Header />
+      <LiveProjects />
     </section>
   );
 }
