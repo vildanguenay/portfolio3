8ca7aa1e509749621b7f817b4a679e73cbe1f4e5
Vildan Günay <vildan-guenay@hotmail.com>
2023-03-07 18:30:06 +0100

    extend mobile menu to tablet, new imgs

--- src/components/About.js
+++ src/components/About.js
@@ -2,33 +2,36 @@ import React from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Card from 'react-bootstrap/Card';
-import Webshop from '.././img/webshop.png';
+import Continental from '.././img/kontinentaldrift.jpg';
+import Communication from '.././img/waterfall.jpg';
+import Presenting from '.././img/cfblur.jpg';
+import Peak from '.././img/peak.jpg';
 
 export const About = () => {
   const itemData = [
     {
-      img: Webshop,
+      img: Presenting,
       eventkey: 'home',
       title: 'Storytelling',
       subTitle: 'Presenting',
       text: 'I enjoy presenting information to people.',
     },
     {
-      img: Webshop,
+      img: Peak,
       eventkey: 'profile',
       title: 'Communication',
       subTitle: 'Journalistic Writing',
       text: "During my bachelor's programme Transcultural Communication creating different text types - mainly journalistic - in three different languages was the main agenda. We have acquired a high level of language and text competence as well as related cultural studies.",
     },
     {
-      img: Webshop,
+      img: Continental,
       eventkey: 'longer-tab',
       title: 'Mediation',
       subTitle: '(International) Conflict Resolution',
       text: "During my masters' programme International Development I developed a firm interest in international peace-keeping. The aim is to remove tensions between parties or states maintaining a consistent level of peaceful and respectful communication in the pursuit of each party's goals.",
     },
     {
-      img: Webshop,
+      img: Communication,
       eventkey: 'contact',
       title: 'Web Development & Design',
       subTitle: '',
