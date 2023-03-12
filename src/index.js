c99a599d8c39945b8f8c4ba298bc129d514e9531
Vildan Günay <vildan-guenay@hotmail.com>
2023-03-12 10:23:12 +0100

    change 2nd path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { Blog } from './components/Blog';
@@ -8,14 +8,12 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/portfolio3" element={<App />}></Route>
-        <Route path="/blog" element={<Blog />}></Route>
-      </Routes>
-    </BrowserRouter>
-  </HashRouter>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/portfolio3" element={<App />}></Route>
+      <Route path="/portfolio3/blog" element={<Blog />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
